6631fb439919a6cde016edbd1bf74d0af53e669b
igordev97 <business.igorstojadinovic97@gmail.com>
2024-03-10 20:52:24 +0100

    Add dark mode for laravel auth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,2 @@
-@extends("layout")
 
-
-@section("content")
-<div class="container mt-5">
-    <div class="row">
-        <div class="col-12">
-
-        </div>
-    </div>
-</div>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -26,10 +26,10 @@
                     <a class="nav-link active" aria-current="page" href="/">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link" href="/login">Login</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
+                    <a class="nav-link" href="/register">Register</a>
                 </li>
 
             </ul>
